2bce4848ae4492b3ffbef4fc1d0675029b373447
unmellow <Amazingminecrafter2015@gmail.com>
2018-09-03 23:54:59 -0400

    i'm learning rust dudes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop{
+        println!("Please enter your guess.");
+
+        let mut guess = String::new();
+    
+        io::stdin().read_line(&mut guess)
+           .expect("Failed to read line WHAT DID YOU DO");
+
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+            println!("You guessed: {}", guess);
+
+            match guess.cmp(&secret_number) {
+                Ordering::Less => println!("To small You Noob!"),
+                Ordering::Greater => println!("Too Big! You dum dum"),
+                Ordering::Equal => {
+                    println!("YOU DID IT THAT'S JUST!! RIGHT");
+                    break;
+                }
+            }
+    }
 }
